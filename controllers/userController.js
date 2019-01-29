2391c7fece610bc33f255db4aac7cfbb3913361b
krushndayshmookh <krushndayshmookh@gmail.com>
2019-01-29 21:34:41 +0530

    get users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
 var User = require('../models/user')
 
 exports.get_users = (req, res) => {
-    res.send(true)
+    User.find({}).exec((err, result) => {
+        if (err) return res.status(500).send(err)
+
+        if (result) return res.send(result)
+
+        return res.send(false)
+
+    })
 }
+
